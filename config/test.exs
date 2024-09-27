5224696f0be36d9f61f25269790e39d637a19a82
Francesco Noacco <francesco.noacco@secomind.com>
2024-09-27 18:52:03 +0200

    test

--- config/test.exs
+++ config/test.exs
@@ -5,4 +5,4 @@ config :cqerl,
   cassandra_nodes: [{cassandra_host, cassandra_port}]
 
 config :astarte_data_access,
-  xandra_nodes: ["#{cassandra_host}:#{cassandra_port}"] 
+  xandra_nodes: "#{cassandra_host}:#{cassandra_port}" 
