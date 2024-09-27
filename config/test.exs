42b94bdbe33f8f45884bce1a16495c223a6e7c4f
Francesco Noacco <francesco.noacco@secomind.com>
2024-09-27 17:00:27 +0200

    wip: test setting xandra_nodes from config
    Signed-off-by: Francesco Noacco <francesco.noacco@secomind.com>

--- config/test.exs
+++ config/test.exs
@@ -1,4 +1,8 @@
 import Config
-
+cassandra_host = System.get_env("CASSANDRA_DB_HOST") || "cassandra"
+cassandra_port = System.get_env("CASSANDRA_DB_PORT") || 9042
 config :cqerl,
-  cassandra_nodes: [{System.get_env("CASSANDRA_DB_HOST") || "cassandra", System.get_env("CASSANDRA_DB_PORT") || 9042}]
+  cassandra_nodes: [{cassandra_host, cassandra_port}]
+
+config :astarte_data_access,
+  xandra_nodes: ["#{cassandra_host}:#{cassandra_port}"] 
