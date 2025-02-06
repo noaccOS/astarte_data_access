cf2d3b8633feec2ece168aeeee9027d7315c33d1
Francesco Noacco <francesco.noacco@secomind.com>
2025-02-06 17:43:18 +0100

    refactor!(realm): port create to exandra
    BREAKING: check_replication is public, and although I don't think
    it's used anywhere, its signature changes as it does not accept
    a connection anymore
    
    Signed-off-by: Francesco Noacco <francesco.noacco@secomind.com>

--- lib/astarte_data_access/astarte/kv_store.ex
+++ lib/astarte_data_access/astarte/kv_store.ex
@@ -19,10 +19,44 @@
 defmodule Astarte.DataAccess.Astarte.KvStore do
   use TypedEctoSchema
 
+  alias Astarte.DataAccess.Repo
+
+  import Ecto.Query
+
   @primary_key false
   typed_schema "kv_store" do
     field :group, :string, primary_key: true
     field :key, :string, primary_key: true
     field :value, :binary
+  end
+
+  @doc """
+    Insert a KvStore, allowing a `Ecto.Query.API` functions as values.
+
+    The Ecto mindset is to finalize the struct on the Elixir side before sending it to \
+    the database. This does not work well with the KvStore, where we need to store many types as \
+    a blob, but don't care about their actual value on the database side.
+
+    By delegating the conversion to the database, we do not need to manually re-implement all the \
+    conversion functions on the elixir side.
+  """
+  @spec insert_with_query(%{group: term(), key: term(), value: term()}, Keyword.t()) :: :ok
+  def insert_with_query(kv_store_map, opts \\ []) do
+    %{
+      group: group,
+      key: key,
+      value: value
+    } = kv_store_map
+
+    insert_query =
+      from kv in __MODULE__,
+        update: [set: [value: ^value]],
+        where: kv.group == ^group and kv.key == ^key
+
+    # We can insert a value using an `update` by pinning the primary key using `where` and
+    # setting the non-key values with update
+    Repo.update_all(insert_query, [], opts)
+
+    :ok
   end
 end
