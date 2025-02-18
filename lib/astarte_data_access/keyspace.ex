ae7573b0183a66090c8a091044a0a9b2e3170773
Francesco Noacco <francesco.noacco@secomind.com>
2025-02-18 10:36:22 +0100

    feat(keyspace)!: port to exandra
    BREAKING: the return type has been modified, as it makes no sense
    in ecto terms to return an :ok/:error tuple when we would always just
    return :ok.
    
    BREAKING: as we were already breaking the signature, the function name
    has been modified
    
    Signed-off-by: Francesco Noacco <francesco.noacco@secomind.com>

--- lib/astarte_data_access/keyspace.ex
+++ lib/astarte_data_access/keyspace.ex
@@ -18,45 +18,19 @@
 
 defmodule Astarte.DataAccess.Keyspace do
   require Logger
-  alias Astarte.DataAccess.XandraUtils
+  alias Astarte.DataAccess.Repo
+  import Ecto.Query
 
-  def keyspace_existing?(keyspace_name) do
-    case XandraUtils.run_without_realm_validation(
-           keyspace_name,
-           fn conn, keyspace_name ->
-             do_keyspace_existing?(conn, keyspace_name)
-           end
-         ) do
-      result when is_boolean(result) ->
-        {:ok, result}
+  def exists?(keyspace_name) do
+    query =
+      from k in "keyspaces",
+        prefix: "system_schema",
+        where: k.keyspace_name == ^keyspace_name,
+        select: k.keyspace_name
 
-      {:error, reason} ->
-        Logger.warning("Cannot select if keyspace existing: #{inspect(reason)}.",
-          tag: "keyspace_existing?",
-          keyspace: keyspace_name
-        )
-
-        {:error, reason}
-    end
-  end
-
-  defp do_keyspace_existing?(conn, keyspace_name) do
-    query = """
-    SELECT
-      COUNT(*)
-    FROM
-      system_schema.keyspaces
-    WHERE
-      keyspace_name = :keyspace_name
-    """
-
-    params = %{
-      keyspace_name: keyspace_name
-    }
-
-    with {:ok, page} <- XandraUtils.retrieve_page(conn, query, params),
-         {:ok, %{count: count}} = Enum.fetch(page, 0) do
-      not (count == 0)
+    case Repo.fetch_one(query) do
+      {:ok, _} -> true
+      {:error, _} -> false
     end
   end
 end
